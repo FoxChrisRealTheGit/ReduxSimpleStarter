e1edbf7598f25aa4147b6e767e9527c18c7c08e9
Christopher Fox <cfox1290@gmail.com>
2018-04-12 19:46:03 -0600

    basic adds

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDOM from 'react-dom'
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = function () {
+    return <div>Hello</div>
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.getElementById("root"))
